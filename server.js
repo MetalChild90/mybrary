8eb072b8b2c25932dfa81177f8b4616ceefb0f08
Eichan <metalchild90@gmail.com>
2020-02-12 19:53:45 +0000

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs'); //że tego chcemy używać
 app.set('views', __dirname + '/views'); //nakierowanie na folder
@@ -15,15 +17,18 @@ app.set('layout', 'layouts/layout'); //że tego chcemy używać
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(cors());
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000); // pierwsze to już bardziej zaawansowane, kiedy jest hosting
